4a94651974ded25914421c24335684ba7148b7cb
unknown <tanidaig@yahoo.co.jp>
2020-01-13 13:43:22 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+var holding = false;
+
+function getRandomInt(min, max) {
+  return min+Math.floor(Math.random() * Math.floor(max-min));
+}
+
+function preload(){
+    
+}
+
+function setup(){
+    createCanvas(600,300);
+    textSize(20);
+    stroke(0,0,0);
+    fill(0,0,0);
+    background(255,255,255);
+}
+
+function mousePressed(){
+    holding = true;
+    
+}
+
+function mouseReleased() {
+    holding = false;
+
+}
+
+function touchStarted(){
+    mousePressed();
+}
+
+function touchEnded(){
+    mouseReleased();
+}
+
+function draw(){
+    
+}
